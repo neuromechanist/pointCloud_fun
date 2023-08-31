d966c1b511950b67cf72936c98d2cf16ea44e914
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-08-31 13:00:55 -0700

    code works fits Delaunay, added interactive plot

--- alpha_fit.py
+++ alpha_fit.py
@@ -12,8 +12,12 @@ points = vtk.vtkPoints()
 for point in point_cloud:
     points.InsertNextPoint(point)
 
+polydata = vtk.vtkPolyData()
+polydata.SetPoints(points)
+
+# %% Create a delaunay triangulation
 delaunay = vtk.vtkDelaunay3D()
-delaunay.SetInputData(points)
+delaunay.SetInputData(polydata)
 delaunay.Update()
 
 # %% Get the surface
@@ -36,3 +40,43 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(points_array[:, 0], points_array[:, 1], points_array[:, 2])
 plt.show()
+
+# %% Create a VTK renderer and window
+renderer = vtk.vtkRenderer()
+renderer.SetBackground(1, 1, 1)  # Set background color to white
+
+render_window = vtk.vtkRenderWindow()
+render_window.AddRenderer(renderer)
+
+# Create a VTK interactor
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(render_window)
+
+# Create a VTK point cloud
+vtk_points = vtk.vtkPoints()
+for point in point_cloud:
+    vtk_points.InsertNextPoint(point)
+
+vtk_polydata = vtk.vtkPolyData()
+vtk_polydata.SetPoints(vtk_points)
+
+# Create a VTK glyph to visualize the points
+glyph = vtk.vtkVertexGlyphFilter()
+glyph.SetInputData(vtk_polydata)
+glyph.Update()
+
+# Create a VTK mapper and actor
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(glyph.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(0, 0, 1)  # Set point color to blue
+
+# Add the actor to the renderer
+renderer.AddActor(actor)
+
+# Start the interactor
+interactor.Initialize()
+render_window.Render()
+interactor.Start()
