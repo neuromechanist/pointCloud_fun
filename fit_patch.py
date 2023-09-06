74a429e2c00a74f774e7fc8cd46c2bf4fae08b81
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-09-05 20:42:36 -0700

    implemented poisson surface, vtk lacking the feature

--- fit_patch.py
+++ fit_patch.py
@@ -119,6 +119,15 @@ for i in range(patch.GetNumberOfPoints()):
     projected_patch.GetPoints().SetPoint(i, projected_point)
 
 
+# Perform surface reconstruction using Poisson algorithm
+poisson = vtk.vtkPoissonReconstruction()
+poisson.SetDepth(10)  # Set the depth parameter for the reconstruction
+poisson.SetInputData(projected_patch)
+poisson.Update()
+
+# Get the output of the Poisson reconstruction
+smooth_surface = poisson.GetOutput()
+
 # %% Create a renderer
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(1, 1, 1)  # Set the background color to white
@@ -143,13 +152,21 @@ surface_actor.SetMapper(surface_mapper)
 surface_actor.GetProperty().SetOpacity(0.7)
 renderer.AddActor(surface_actor)
 
-# Add the projected patch to the renderer
-projected_patch_mapper = vtk.vtkPolyDataMapper()
-projected_patch_mapper.SetInputData(projected_patch)
-projected_patch_actor = vtk.vtkActor()
-projected_patch_actor.SetMapper(projected_patch_mapper)
-projected_patch_actor.GetProperty().SetColor(0, 1, 0)  # Set the color to green
-renderer.AddActor(projected_patch_actor)
+# # Add the projected patch to the renderer
+# projected_patch_mapper = vtk.vtkPolyDataMapper()
+# projected_patch_mapper.SetInputData(projected_patch)
+# projected_patch_actor = vtk.vtkActor()
+# projected_patch_actor.SetMapper(projected_patch_mapper)
+# projected_patch_actor.GetProperty().SetColor(0, 1, 0)  # Set the color to green
+# renderer.AddActor(projected_patch_actor)
+
+# Add the smooth surface to the renderer
+smooth_surface_mapper = vtk.vtkPolyDataMapper()
+smooth_surface_mapper.SetInputData(smooth_surface)
+smooth_surface_actor = vtk.vtkActor()
+smooth_surface_actor.SetMapper(smooth_surface_mapper)
+smooth_surface_actor.GetProperty().SetColor(0, 1, 0)  # Set the color to green
+renderer.AddActor(smooth_surface_actor)
 
 # Render the scene
 render_window.Render()
