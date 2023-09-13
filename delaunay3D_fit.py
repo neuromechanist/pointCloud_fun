a8618df4c9bc99dee43ea7f689310432a22eceb1
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-09-13 16:59:35 -0700

    made pointcloud and delaunay plot together, very neat

--- delaunay3D_fit.py
+++ delaunay3D_fit.py
@@ -1,3 +1,4 @@
+# %% init
 import vtk
 import numpy as np
 
@@ -29,22 +30,59 @@ surface_filter.Update()
 # Get the surface as vtkPolyData
 surface = surface_filter.GetOutput()
 
-# %% Visualize the surface
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(surface)
+# %% Create the surface renderer
+surface_mapper = vtk.vtkPolyDataMapper()
+surface_mapper.SetInputData(surface)
 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-
+surface_actor = vtk.vtkActor()
+surface_actor.SetMapper(surface_mapper)
+surface_actor.GetProperty().SetOpacity(0.4)
 renderer = vtk.vtkRenderer()
-renderer.AddActor(actor)
+renderer.AddActor(surface_actor)
+renderer.SetBackground(1, 1, 1)  # Set the background color to white
 
 render_window = vtk.vtkRenderWindow()
+render_window.AddRenderer(renderer)
+
+# Create a VTK point cloud
+vtk_points = vtk.vtkPoints()
+for point in point_cloud:
+    vtk_points.InsertNextPoint(point)
+
+vtk_polydata = vtk.vtkPolyData()
+vtk_polydata.SetPoints(vtk_points)
+
+# Create a VTK glyph to visualize the points
+glyph = vtk.vtkGlyph3D()
+glyph.SetInputData(vtk_polydata)
+glyph.Update()
+
+# set the shape of the glyph
+sphere_source = vtk.vtkSphereSource()
+sphere_source.SetRadius(0.25)  # Set the radius of the sphere
+glyph.SetSourceConnection(sphere_source.GetOutputPort())
+glyph.Update()
+
+# Increase the glyph size
+# glyph.SetScaleFactor(2.0)  # Adjust the scale factor as desired
+
+# Create a VTK mapper and actor
+pc_mapper = vtk.vtkPolyDataMapper()
+pc_mapper.SetInputConnection(glyph.GetOutputPort())
+
+pc_actor = vtk.vtkActor()
+pc_actor.SetMapper(pc_mapper)
+pc_actor.GetProperty().SetColor(0, 0, 1)  # Set point color to blue
+
+# Add the actor to the renderer
+renderer.AddActor(pc_actor)
+
 render_window.AddRenderer(renderer)
 
 interactor = vtk.vtkRenderWindowInteractor()
 interactor.SetRenderWindow(render_window)
 
+# %% Create thea renderer
 interactor.Initialize()
 render_window.Render()
 interactor.Start()
