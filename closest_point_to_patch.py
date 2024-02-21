31413f48b2ff3e7b2229e5c8d7184251f6d4faf3
Seyed Yahya Shirazi <shirazi@ieee.org>
2024-02-21 12:24:06 -0800

    Find unique closest points for each patch

--- closest_point_to_patch.py
+++ closest_point_to_patch.py
@@ -11,7 +11,7 @@ import vtk
 import numpy as np
 from scipy.spatial import cKDTree
 
-PLOT_PATCH = True
+PLOT_PATCH = False
 # load the patch
 # Create a vtkPLYReader object
 ply_reader = vtk.vtkPLYReader()
@@ -88,13 +88,13 @@ bounds = patch.GetBounds()
 bounds_range = [bounds[1] - bounds[0], bounds[3] - bounds[2], bounds[5] - bounds[4]]
 
 # divide the patch to 32x32 smaller patches
-n_patches = 32  # number of patches in each direction
-PATCH_SIZE = (np.array(bounds_range) / n_patches).tolist()
+NUM_PATCHES = 32  # number of patches in each direction
+PATCH_SIZE = (np.array(bounds_range) / NUM_PATCHES).tolist()
 patch_bounds = []
 patch_centers = []
 
-for i in range(n_patches):
-    for j in range(n_patches):
+for i in range(NUM_PATCHES):
+    for j in range(NUM_PATCHES):
         patch_bounds.append([bounds[0] + i * PATCH_SIZE[0], bounds[0] + (i + 1) * PATCH_SIZE[0],
                              bounds[2] + j * PATCH_SIZE[1], bounds[2] + (j + 1) * PATCH_SIZE[1],
                              bounds[4], bounds[5]])
@@ -156,7 +156,6 @@ if PLOT_PATCH:
     centerpoint_actor.SetMapper(centerpoint_mapper)
     centerpoint_actor.GetProperty().SetColor(1, 0, 0)  # Set point color to red
 
-
     # create a renderer
     renderer = vtk.vtkRenderer()
     renderer.SetBackground(0.5, 0.5, 0.5)  # Set the background color to white
@@ -181,7 +180,8 @@ if PLOT_PATCH:
 tree = cKDTree(point_cloud)
 
 num_patches = len(patch_centers)
-NUMBER_OF_CLOSEST_POINTS = 3
+NUMBER_OF_CLOSEST_POINTS = 10
+closest_points_index = np.zeros((num_patches, NUMBER_OF_CLOSEST_POINTS))
 closest_points_to_patch = np.zeros((num_patches, NUMBER_OF_CLOSEST_POINTS, 3))
 distance_of_closest_points = np.zeros((num_patches, NUMBER_OF_CLOSEST_POINTS))
 
@@ -190,5 +190,29 @@ for i, c in enumerate(patch_centers):
     distance, index = tree.query(c, NUMBER_OF_CLOSEST_POINTS)
 
     # get the closest points
+    closest_points_index[i, :] = index
     closest_points_to_patch[i, :, :] = point_cloud[index, :]
     distance_of_closest_points[i, :] = distance
+
+# %% choose unique points for each patch
+# Each smaller path should only correspond to one point in the point cloud.
+# For each patch, if use the next closest point if the closest point is already used for the previous patches.
+# If the next closest point is also used, use the next closest point.
+
+# initialize the index of the closest points
+unique_closest_point_index = np.zeros(num_patches, dtype=int)
+unique_closest_point_to_patch = np.zeros((num_patches, 3))
+unique_distance_of_closest_point = np.zeros(num_patches)
+
+# initialize the used points
+used_points_index = np.zeros(NUM_PATCHES**2)
+
+for i in range(num_patches):
+    # find if the closest points are used before
+    used_flag = [True if index in used_points_index else False for index in closest_points_index[i, :]]
+    # find the index of the closest point that is not used before
+    if not all(used_flag):
+        unique_closest_point_index[i] = closest_points_index[i, np.where(np.logical_not(used_flag))[0][0]]
+        unique_closest_point_to_patch[i, :] = closest_points_to_patch[i, np.where(np.logical_not(used_flag))[0][0], :]
+        unique_distance_of_closest_point[i] = distance_of_closest_points[i, np.where(np.logical_not(used_flag))[0][0]]
+        used_points_index[i] = unique_closest_point_index[i]
